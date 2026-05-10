194a8d4739c6245acdaf0b0601ac03c0f41f1d83
devxiexie <eric.98026@gmail.com>
2026-05-09 21:00:37 -0700

    content, new cursor

--- script.js
+++ script.js
@@ -8,4 +8,22 @@ window.addEventListener("DOMContentLoaded", () => {
 
     el.style.setProperty("--delay", `${delay}s`);
   });
+
+  const cursor = document.createElement("div");
+  cursor.className = "fake-cursor";
+  document.body.appendChild(cursor);
+
+  window.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+    cursor.classList.add("visible");
+  });
+
+  document.documentElement.addEventListener("mouseleave", () => {
+    cursor.classList.remove("visible");
+  });
+
+  document.documentElement.addEventListener("mouseenter", () => {
+    cursor.classList.add("visible");
+  });
 });
